2e35ad21064d2ed3ad70077cf87bb47970b22dfa
yuenkev <yuenkev@sheridancollege.ca>
2022-01-21 09:52:47 -0500

    Reverting Array Changes.

--- src/studentlist/studentList.java
+++ src/studentlist/studentList.java
@@ -11,9 +11,7 @@ package studentlist;
  */
 public class studentList {
     public static void main(String[] args){
-        
-        Student[] studentList = new Student[4];
-        
+        Student[] studentList = new Student[3];
         studentList[0] = new Student("1","Ronak Sheth");
         studentList[1] = new Student("2","John Doe");
         studentList[2] = new Student("3","abc");

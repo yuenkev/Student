bc4b0be864b1f8913578d9f2efa1307ec8c20ace
yuenkev <yuenkev@sheridancollege.ca>
2022-01-28 10:26:59 -0500

    Second Commit: part-time-student added

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author ktyue
+ */
+public class PartTimeStudent extends Student {
+    private int numCourses; 
+    
+    public PartTimeStudent(String studentId, String studentName, int numCourses){
+        super(studentId, studentName);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+}

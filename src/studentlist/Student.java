4376390ee163a06ca0923009c0d44c653d894320
Kevin Yuen <90118768+yuenkev@users.noreply.github.com>
2022-01-28 09:47:46 -0500

    Added Comments
    Author Name + Date

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Author: Kevin Yuen
+Date: January 28, 2022
  */
 package studentlist;
 
